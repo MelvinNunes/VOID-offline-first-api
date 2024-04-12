1f24c89a386d34b8b07cc307216142b907c9195c
Melvin Nunes <melvinnunes14@gmail.com>
2024-04-12 16:22:12 +0200

    fix: health test

--- test/health.test.ts
+++ test/health.test.ts
@@ -2,11 +2,10 @@ import request from "supertest";
 const app = require("../src/application/index");
 
 describe("Test API Health", () => {
-  it("responds to /api/health", async () => {
+  it("responds to /api/v1/health", async () => {
     const res = await request(app)
-      .get("/api/health")
+      .get("/api/v1/health")
       .set("Accept", "application/json");
     expect(res.status).toBe(200);
-    expect(res.body).toEqual({ message: "API is health!." });
   });
 });
