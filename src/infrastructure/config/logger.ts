3104a1d3fd89a36d8e736f00a8aaf8d8ad3cc1a7
Melvin Nunes <melvinnunes14@gmail.com>
2024-04-12 22:13:49 +0200

    fix: logger added by date

--- src/infrastructure/config/logger.ts
+++ src/infrastructure/config/logger.ts
@@ -1,10 +1,13 @@
 import winston from "winston";
 
+const date = new Date();
+const fileName = `${date.toISOString().slice(0, 10)}-void.log`;
+
 export const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/app.log" }),
+    new winston.transports.File({ filename: `logs/${fileName}` }),
   ],
 });
